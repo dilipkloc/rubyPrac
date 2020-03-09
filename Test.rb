3c016ec656cada9652e996f8f33055edb5121e31
dilipkloc <dilipkloctech@gmail.com>
2020-03-09 12:14:28 +0530

    JSON only with ruby practiced

---
+++ Test.rb
@@ -0,0 +1,23 @@
+require 'rubygems'
+require 'httparty'
+
+class Test
+  include HTTParty
+  base_uri 'https://jsonplaceholder.typicode.com'
+  def posts
+    self.class.get('/posts');
+  end
+end
+
+test = Test.new
+response = test.posts
+# puts response
+response.each do |value|
+    value.each do |key,value1|
+        p = key.to_s+":"+value1.to_s+"\t"
+        puts p
+    end
+    puts "\n\n"
+end
+# puts response.is_a?(Object)
+# response.each(|key, value|)
