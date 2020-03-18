dcc1817c18cf6a6611c3f7322aa6639a933de610
dilipkloc <dilipkloctech@gmail.com>
2020-03-18 07:48:11 +0530

    fix: file renamed Test -> JsonFile

--- forTest.rb
+++ forTest.rb
@@ -1,19 +1,44 @@
 require 'json'
-class User
-    attr_accessor :name,:clan,:honor,:temp
-    def initialize(name,clan,honor)
-      @clan = clan
-      @honor = honor
-      @name = name
+require 'pry'
+require 'nokogiri'
+require 'open-uri'
+class Parser
+    URL = 'https://www.codewars.com/users/leaderboard'.freeze
+    def initialize
+      @content = Nokogiri::HTML(open(URL), nil, 'UTF-8')
     end
-  
-    def test
-      JSON.parse({ 
-        :name => @name, 
-        :clan => @clan, 
-        :honor => @honor
-    }.to_json)
+    def rows
+      @rows ||= @content.css('table').css('tr')[1..500]
     end
 end
+class User
+    attr_reader :name, :clan, :honor
+    def initialize(name:, clan:, honor:)
+      @name, @clan, @honor = name, clan, honor
+    end
+  end
+class Leaderboard
+    def initialize
+      @parser = Parser.new
+    end
+    def position
+      @position= prepare_users
+    end
+    private
+    attr_reader :parser
+    def prepare_users
+      users = []
+      parser.rows.each do |row|
+        users.push(
+          User.new(
+            name: row['data-username'],
+            clan: row.css('td')[2].text,
+            honor: row.css('td').last.text
+          )
+        )
+      end
+      return users
+    end
+  end
 
 #   p User.new('asha','yerume',12).test
