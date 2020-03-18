7cf25d5b247280797274620d0568ecfc47aac003
dilipkloc <dilipkloctech@gmail.com>
2020-03-18 08:28:00 +0530

    json place holder practiced

--- JSON_Practice.rb
+++ JSON_Practice.rb
@@ -4,23 +4,42 @@ require 'httparty'
 class Test
   include HTTParty
   base_uri 'https://jsonplaceholder.typicode.com'
-  def posts
-    self.class.get('/posts');
+  def get(method, id = -1)
+    if id == -1
+      self.class.get('/'+method);
+    else
+      self.class.get('/'+method+'/'+id);
+    end
   end
 end
 
-test = Test.new
-response = test.posts
+# response = Test.new.get('posts')
+# response = Test.new.get('comments')
+# response = Test.new.get('comments',23.to_s)
+# response = Test.new.get('albums')
+# response = Test.new.get('photos')
+# response = Test.new.get('todos')
+response = Test.new.get('users')
 
 
 
-# puts response
-response.each do |value|
-    value.each do |key,value1|
-        p = key.to_s+":"+value1.to_s+"\t"
-        puts p
-    end
-    puts "\n\n"
+
+
+File.open('file_json_complete.json', 'w') do |f|
+  f.puts JSON.parse(response.to_json)
 end
-# puts response.is_a?(Object)
-# response.each(|key, value|)
+
+
+
+
+# Below is used to print in colsole(ignore below code)
+# puts response
+# response.each do |value|
+#   p value
+#   puts "\n\n"
+#     # value.each do |key,value1|
+#     #     p = key.to_s+":"+value1.to_s+"\t"
+#     #     puts p
+#     # end
+#     # puts "\n\n"
+# end
